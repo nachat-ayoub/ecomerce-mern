3e634a88dd60949b015a61f4284e8f36e9c19f16
venomVV <venomx2020x@gmail.com>
2022-01-23 14:00:36 +0100

    pushing repo

--- backend/server.js
+++ backend/server.js
@@ -24,25 +24,6 @@ app.use(cors());
 // Routes :
 const AuthRoutes = require("./routes/authRoutes");
 
-app.get("/d", (req, res) => {
-  res.send(`<!DOCTYPE html>
-  <html>
-  
-  <body>
-  
-  <img width="100%" src='http://api.page2images.com/directlink?p2i_url=https://www.youtube.com/results?search_query=how+to+use+ffmpeg+in+heroku&p2i_key=${process.env.APIKEYD}&p2i_device=0'>
-  
-  <script src="http://www.page2images.com/js/p2i.js"></script>
-  <script>
-    const p2i = new page2images();
-    p2i.thumbnail('demo');
-  </script>
-  
-  </body>
-  
-  </html>
-  `);
-});
 // Start : Login Page And Logic :
 app.use("/", AuthRoutes);
 
